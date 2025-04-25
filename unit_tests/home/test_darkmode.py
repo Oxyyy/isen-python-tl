461098dc728bbe327126430f1ce8be992786e55a
Oxyyy <theo.leroux@isen.yncrea.fr>
2025-04-25 13:16:06 +0200

    fix: test dark mode

--- unit_tests/home/test_darkmode.py
+++ unit_tests/home/test_darkmode.py
@@ -4,16 +4,20 @@ from django.urls import reverse
 class DarkModeTest(TestCase):
     def setUp(self):
         self.client = Client()
+        self.response = self.client.get(reverse('home'))
 
-    def test_homepage_contains_darkmode_toggle(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'id="darkModeToggle"')
+    def test_status_code(self):
+        """La page d'accueil répond correctement"""
+        self.assertEqual(self.response.status_code, 200)
 
-    def test_homepage_contains_darkmode_script(self):
-        response = self.client.get(reverse('home'))
-        self.assertContains(response, 'function setDarkMode()')
+    def test_contains_darkmode_toggle_button(self):
+        """Le bouton de bascule Dark Mode est présent"""
+        self.assertContains(self.response, 'id="darkModeToggle"')
 
-    def test_homepage_contains_darkmode_class(self):
-        response = self.client.get(reverse('home'))
-        self.assertContains(response, 'dark-mode')
+    def test_contains_darkmode_script_function(self):
+        """La fonction setDarkMode est présente dans le script JS"""
+        self.assertContains(self.response, 'function setDarkMode(')
+
+    def test_contains_darkmode_class_in_css(self):
+        """La classe CSS .dark-mode est bien définie"""
+        self.assertContains(self.response, 'body.dark-mode')
